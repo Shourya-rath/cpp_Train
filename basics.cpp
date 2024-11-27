aa4418b0b887ade8b9435eb32917fc3ebf0ecbfc
Shourya-rath <rathsdsr@gmail.com>
2024-11-27 21:55:10 +0530

    basics

---
+++ basics.cpp
@@ -0,0 +1,30 @@
+/*
+ n natural numbers -> (n (n +1)) /2
+ simple interest -> (p * t * r) / 100 
+concept -> cin ,cout
+
+>> compound assignment , increment , decrement
+( TODO )not used too much: 
+    Bitwise Operators
+
+    Demo - Bitwise Operators
+
+    Enum and Typedef
+
+compound assignment -> convenience 
+    x = x + 1 ;
+    x += 1 ;
+
+    x = x + 2 ;
+    x += 2 ;
+    wont work : x++ ;
+increment , decrement
+    x = 1 ;
+    x++ ;
+    ++x ;
+    a = x++ ;
+    cout << a << x ;
+    b = ++x ;
+postfix -> x++, prefix ->   ++x 
+
+*/
---
+++ incre_decre.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x = 1, y = 1;
+// int a = x++ , b = ++y;
+// cout << a << b; // a= 1 , b = 2 
+
+int a = x ;
+x += 1 ;  
+int b = x + 1 ;  
+
+cout << a << b << x;
+for (int i = 0 ;
+    i > 0 ;
+    i--){
+}
